fc0d2c10c1fdcbd80d8b9b6bee32e938beead0f0
Aditya Eka <wildslashx@gmail.com>
2016-11-25 21:30:45 +0700

    commit Eka - menambahkan fitur nomor 6 dan 7

---
+++ lamaran/lamaran_view.php
@@ -0,0 +1,110 @@
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+  <script type="text/javascript" src="jquery-1.7.2.min.js"></script>
+  <script type="text/javascript" src="functions.js"></script>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.0"/>
+  <title>Starter Template - Materialize</title>
+
+  <!-- CSS  -->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <link href="../css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+  <link href="../css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+</head>
+<body>
+  <nav class="light-blue lighten-1" role="navigation">
+    <div class="nav-wrapper container">
+      <a href="welcome.php" id="logo-container" href="#" class="brand-logo">Home</a>
+      <ul class="right hide-on-med-and-down">
+        <li><a href="#">Lowongan</a></li>
+        <li><a href="#">Profil<l/a></li>
+        <li><a href="#">Log</a></li>
+      </ul>
+   </div>
+ </nav>
+  <div class="section no-pad-bot" id="index-banner">
+    <div class="container">
+      <br><br>
+      <h1 class="header center orange-text">Daftar Pelamar Lowongan</h1>
+	  <h3 class="header center orange-text">Kelas Basis Data Lanjut, 2016-2017, Ganjil</h3>
+      <div class="row center">
+        <div style='overflow-x:auto'>
+          <table class='highlight bordered'>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>Nama</th>
+              <th>NPM</th>
+              <th>Email</th>
+              <th>Profil</th>
+              <th>Status</th> 
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Andi Sartono</td>
+              <td>12004950234</td>
+              <td>andi@sartono.com</td>
+              <td><u>Lihat</u></td>
+              <td>melamar</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Budi Sanjaya</td>
+              <td>12004950336</td>
+              <td>me@sanjaya.id</td>
+              <td><u>Lihat</u></td>
+              <td>diterima</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Chanek</td>
+              <td>12004984552</td>
+              <td>chanek@gmail.com</td>
+              <td><u>Lihat</u></td>
+              <td>diterima</td>
+            </tr>
+            <tr>
+              <td>4</td>
+              <td>Halimo</td>
+              <td>12004945479</td>
+              <td>halimo@yahoo.com</td>
+              <td><u>Lihat</u></td>
+              <td>melamar</td>
+            </tr>
+            <tr>
+              <td>5</td>
+              <td>Sogo Eka Risti</td>
+              <td>12004957631</td>
+              <td>sogo@hotmail.com</td>
+              <td><u>Lihat</u></td>
+              <td>melamar</td>
+            </tr>
+            <tr>
+              <td>6</td>
+              <td>Willy Limpo</td>
+              <td>12004912357</td>
+              <td>Willy@limpo.co.id</td>
+              <td><u>Lihat</u></td>
+              <td>diterima</td>
+            </tr>
+          </tbody>
+        </table>
+        </div>
+        <br>
+    </div>
+
+
+
+
+
+
+    <!--  Scripts-->
+    <script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+    <script src="js/materialize.js"></script>
+    <script src="js/init.js"></script>
+
+  </body>
+  </html>
