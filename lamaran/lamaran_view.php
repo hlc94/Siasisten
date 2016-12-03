6ffd6a23b74099700152b9b62d2938e28cec5976
Aditya Eka <wildslashx@gmail.com>
2016-12-03 20:39:05 +0700

    upload lamaran_view.php

--- lamaran/lamaran_view.php
+++ lamaran/lamaran_view.php
@@ -30,68 +30,46 @@
 	  <h3 class="header center orange-text">Kelas Basis Data Lanjut, 2016-2017, Ganjil</h3>
       <div class="row center">
         <div style='overflow-x:auto'>
-          <table class='highlight bordered'>
-          <thead>
-            <tr>
-              <th>No.</th>
-              <th>Nama</th>
-              <th>NPM</th>
-              <th>Email</th>
-              <th>Profil</th>
-              <th>Status</th> 
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>Andi Sartono</td>
-              <td>12004950234</td>
-              <td>andi@sartono.com</td>
-              <td><u>Lihat</u></td>
-              <td>melamar</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>Budi Sanjaya</td>
-              <td>12004950336</td>
-              <td>me@sanjaya.id</td>
-              <td><u>Lihat</u></td>
-              <td>diterima</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>Chanek</td>
-              <td>12004984552</td>
-              <td>chanek@gmail.com</td>
-              <td><u>Lihat</u></td>
-              <td>diterima</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>Halimo</td>
-              <td>12004945479</td>
-              <td>halimo@yahoo.com</td>
-              <td><u>Lihat</u></td>
-              <td>melamar</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>Sogo Eka Risti</td>
-              <td>12004957631</td>
-              <td>sogo@hotmail.com</td>
-              <td><u>Lihat</u></td>
-              <td>melamar</td>
-            </tr>
-            <tr>
-              <td>6</td>
-              <td>Willy Limpo</td>
-              <td>12004912357</td>
-              <td>Willy@limpo.co.id</td>
-              <td><u>Lihat</u></td>
-              <td>diterima</td>
-            </tr>
-          </tbody>
-        </table>
+          <?php
+            $namadosen=$_SESSION['username'];
+            $query="SELECT m.nama, m.npm, m.email, sl.status
+					FROM mahasiswa m, lamaran lam, status_lamaran sl
+					WHERE 
+					m.NPM = lam.NPM AND
+					lam.ID_st_lamaran = sl.ID;";
+            $result = $connect->query($query);
+            if ($result->num_rows > 0) {
+              echo 
+                "<table class='highlight bordered'>
+                <thead>
+                  <tr>
+                    <th>No.</th>
+                    <th>Nama</th>
+                    <th>NPM</th>
+                    <th>Email</th>
+                    <th>Profil</th>
+                    <th>Status</th> 
+                  </tr>
+                </thead>
+                <tbody>";
+              while($row = $result->fetch_assoc()) {
+              echo 
+                "<tr>
+                  <td>
+					<?php
+						echo $i;
+						$i++;
+					?>
+				  </td>
+                  <td>".$row["nama]."</td>
+                  <td>".$row["npm"]."</td>
+                  <td>".$row["email"]."</td>
+                  <td><a href='buka_lowongan.php?id=".$row["npm"]."'>Lihat</a></td>
+                  <td>".$row["status"]."</td>
+                </tr>
+              </tbody>
+            </table>";
+          ?>
         </div>
         <br>
     </div>
