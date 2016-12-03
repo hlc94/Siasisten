db03e7ccc56fd6d928b0526d85b5d34da564d3ef
hlc94 <henrylouisc@gmail.com>
2016-12-03 11:54:30 +0700

    Update dbconfig.php

--- dbconfig.php
+++ dbconfig.php
@@ -1,8 +1,14 @@
-<?php
-$host = "localhost";
-$user = "root";
-$password = "";
+/*<?php
+$host = "db.cs.ui.ac.id";
+$user = "henry.louis ";
+$password = "KanbanMusume01";
 $datbase = "dbtuts";
-mysql_connect($npm,$password);
+p_connect($npm,$password);
 mysql_select_db($datbase);
+?>*/
+
+<?php
+	$conn = pg_connect("host=db.cs.ui.ac.id user=henry.louis password=KanbanMusume01");
+	$sql = "set search_path to siasisten;";
+	$result = pg_query($conn,$sql) or die("Query failed".pg_last_error());	
 ?>
